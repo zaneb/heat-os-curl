3705c25e27d5117b1c011a443380ad58ba9e01b6
Zane Bitter <zbitter@redhat.com>
2012-10-22 13:50:10 +0200

    Use tenant ID instead of name

--- config.sh
+++ config.sh
@@ -2,5 +2,10 @@ PORT=8004
 ENDPOINT=/v1
 #export HOST=localhost
 
+get_tenant_id() {
+    local tenant_name=$1
+    echo $(keystone tenant-list | awk -F'|' "\$3 ~ \"^ ${tenant_name} *\$\" { print \$2 }")
+}
+
 . ~/.openstack/keystonerc
-TENANT=$OS_TENANT_NAME
+TENANT=`get_tenant_id "$OS_TENANT_NAME"`
